a503204a689140705afe4066125d644db5ff2421
Kiruthika <kiruthikannc25@gmail.com>
2026-04-17 15:04:56 +0530

    build error fix

--- src/component/loader/Loader.jsx
+++ src/component/loader/Loader.jsx
@@ -1,24 +1,10 @@
 import React from "react";
-// import { ColorRing, DNA, LineWave } from "react-loader-spinner";
 import "../loader/loader.css";
 
 const Loader = () => {
   return (
     <div className="loader-container">
-      <div className="loader">
-        {/* <LineWave
-          visible={true}
-          height="100"
-          width="100"
-          color="#4fa94d"
-          ariaLabel="line-wave-loading"
-          wrapperStyle={{}}
-          wrapperClass=""
-          firstLineColor=""
-          middleLineColor=""
-          lastLineColor=""
-        /> */}
-      </div>
+      <div className="loader" />
     </div>
   );
 };
